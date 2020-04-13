5fe91ac6519f10d36c922f0d720e10b9d44a0feb
Jochum Börger <jborger@xebia.com>
2020-04-13 15:32:43 +0200

    Add kotlin/gradle project setup

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    kotlin("jvm") version "1.3.71"
+    id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
+}
+
+group = "nl.jochumborger.durak"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation(kotlin("reflect"))
+
+    testImplementation("org.spekframework.spek2:spek-dsl-jvm:2.0.9")
+    testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:2.0.9")
+}
+
+tasks {
+    test {
+        useJUnitPlatform {
+            includeEngines("spek2")
+        }
+    }
+
+    compileKotlin {
+        kotlinOptions.jvmTarget = "11"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "11"
+    }
+}
