4498d0f862e22dce7c2a45360b08820e5e43c20b
Jochum Börger <jborger@xebia.com>
2020-04-14 10:48:39 +0200

    Refactor tests

--- src/test/kotlin/nl/jochumborger/durak/domain/DeckSpec.kt
+++ src/test/kotlin/nl/jochumborger/durak/domain/DeckSpec.kt
@@ -8,6 +8,10 @@ import strikt.assertions.isGreaterThan
 import strikt.assertions.isLessThan
 import strikt.assertions.isTrue
 
+private const val NUM_RANKS = 9 // 6 upto Ace
+private const val NUM_SUITS = 4
+private const val DECK_SIZE = NUM_RANKS * NUM_SUITS
+
 object DeckSpec : Spek({
     describe("A Deck of cards") {
         var deck: Deck = Deck.createShuffledDeck()
@@ -16,33 +20,33 @@ object DeckSpec : Spek({
             deck = Deck.createShuffledDeck()
         }
 
-        it("has 36 cards") {
-            expectThat(deck.size()).isEqualTo(36)
+        it("has $DECK_SIZE cards") {
+            expectThat(deck.size()).isEqualTo(DECK_SIZE)
         }
 
-        it("has 9 ranks, 6 upto Ace (14), and all 4 suits") {
-            val allCards = (1..36).mapNotNull { deck.pick() }
-            expectThat(allCards.map { it.rank.value }.min()).isEqualTo(6)
+        it("has $NUM_RANKS ranks upto Ace, and $NUM_SUITS suits") {
+            val allCards = (1..DECK_SIZE).mapNotNull { deck.pick() }
+            expectThat(allCards.groupBy { it.rank }.size).isEqualTo(NUM_RANKS)
             expectThat(allCards.maxBy { it.rank.value }!!.rank).isEqualTo(Card.Rank.ACE)
-            expectThat(allCards.groupBy { it.rank }.size).isEqualTo(9)
-            expectThat(allCards.groupBy { it.suit }.size).isEqualTo(4)
+            expectThat(allCards.map { it.rank.value }.min()).isEqualTo(Card.Rank.ACE.value - NUM_RANKS + 1)
+            expectThat(allCards.groupBy { it.suit }.size).isEqualTo(NUM_SUITS)
         }
 
         it("has no duplicate cards") {
-            val allDistinctCards = (1..36).mapNotNull { deck.pick() }.onEach { println(it) }.toSet()
-            expectThat(allDistinctCards.size).isEqualTo(36)
+            val allDistinctCards = (1..DECK_SIZE).mapNotNull { deck.pick() }.onEach { println(it) }.toSet()
+            expectThat(allDistinctCards.size).isEqualTo(DECK_SIZE)
             expectThat(deck.isEmpty()).isTrue()
         }
 
         it("is not clustered by suit") {
-            val chunks = (1..36).mapNotNull { deck.pick() }.chunked(9)
+            val chunks = (1..DECK_SIZE).mapNotNull { deck.pick() }.chunked(NUM_RANKS)
             chunks.forEach {
                 expectThat(it.groupBy { c -> c.suit }.keys.size).isGreaterThan(1)
             }
         }
 
         it("is not clustered by rank") {
-            val chunks = (1..36).mapNotNull { deck.pick() }.chunked(4)
+            val chunks = (1..DECK_SIZE).mapNotNull { deck.pick() }.chunked(NUM_SUITS)
             chunks.forEach {
                 expectThat(it.groupBy { c -> c.rank }.keys.size).isGreaterThan(1)
             }
@@ -50,8 +54,8 @@ object DeckSpec : Spek({
 
         it("is not equal to another shuffled deck") {
             val deck2 = Deck.createShuffledDeck()
-            val equalCards = (1..36).filter { deck.pick() == deck2.pick() }.size
-            expectThat(equalCards).isLessThan(36)
+            val equalCards = (1..DECK_SIZE).filter { deck.pick() == deck2.pick() }.size
+            expectThat(equalCards).isLessThan(DECK_SIZE)
         }
     }
 })
